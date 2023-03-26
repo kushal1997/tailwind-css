c565b008b6c46a0e315600d6c207d253ac426367
kushal1997 <kushalrao103@gmail.com>
2023-03-26 20:45:54 +0530

    played with tailwind css clases

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
+    <div className="App border-2 border-blue-700 border-solid rounded-md">
+      <h1 className='text-5xl bg-orange-400 text-center py-7 m-5'>Learning Tailwind</h1>
+      <p>Hello World</p>
+      <p>Learning <span className='text-2xl font-bold underline'> Tailwind </span> with kushal !</p>
     </div>
+    
   );
 }
 
