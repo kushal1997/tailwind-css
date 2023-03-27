3ae53131a9187d2660d721ebf76c1920e554d3de
kushal1997 <kushalrao103@gmail.com>
2023-03-27 07:44:01 +0530

    played more with custom classes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    
-    <div className="App border-2 border-blue-700 border-solid rounded-md">
-      <h1 className='text-5xl bg-orange-400 text-center py-7 m-5'>Learning Tailwind</h1>
-      <p>Hello World</p>
-      <p>Learning <span className='text-2xl font-bold underline'> Tailwind </span> with kushal !</p>
+
+    <main>
+      <div className="App border-2 border-blue-700 border-solid rounded-md">
+      <h1 className='text-5xl cursor-wait bg-orange-400 hover:bg-red-400 text-center py-7 m-5'>Learning Tailwind</h1>
+      <p className='text-2xl bg-logo-200 md:bg-logo-800 lg:bg-red-600 p-2 m-2'>Hello World</p>
+      <p className='bg-logo-200 dark:bg-logo-800'>Learning <span className='text-2xl font-bold underline'> Tailwind </span> with kushal !</p>
+      
     </div>
-    
+    </main>
+
   );
 }
 
